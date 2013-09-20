ba7f1f19ff8c99c32b3e1c69cf55158deee0218f
Cesar Leblic <cesar.leblic@gmail.com>
2013-09-20 12:33:01 +0200

    GameHelper static class

---
+++ MonoFramework/MonoFramework/GameHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoFramework
+{
+    public static class GameHelper
+    {
+        private static Random rand_;
+
+        public static int RandomNext(int maxValue)
+        {
+            return RandomNext(0, maxValue);
+        }
+
+        public static int RandomNext(int min, int max)
+        {
+            if (rand_ == null)
+                rand_ = new Random();
+            return rand_.Next(min, max);
+        }
+
+        public static float RandomNext(float max)
+        {
+            return RandomNext(0.0f, max);
+        }
+
+        public static float RandomNext(float min, float max)
+        { 
+            if (rand_ == null)
+                rand_ = new Random();
+            return (float)rand_.NextDouble() * (max - min) + min;
+
+        }
+    }
+}
