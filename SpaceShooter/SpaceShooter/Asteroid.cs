9feff723f271f41b9544f50f10eef5f86c8cadb9
Cesar Leblic <cesar.leblic@gmail.com>
2013-09-23 11:23:36 +0200

    Begging of a simple space shooter game -> Asteroid Class

---
+++ SpaceShooter/SpaceShooter/Asteroid.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoFramework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceShooter
+{
+    public class Asteroid : SpriteObject
+    {
+        private static string[] asteroidTextures = new string[] {"Rock1", "Rock2", "Rock3"};
+
+        public Asteroid(GameHost game)
+            : base(game)
+        {
+            RotateSpeed = GameHelper.RandomNext(-0.5f, 0.5f);
+            Speed = GameHelper.RandomNext(-2f, 2f);
+            Life = 3;
+            do
+            {
+                Direction = new Vector2(GameHelper.RandomNext(0f, 1f), GameHelper.RandomNext(0f, 1f));
+            } while (Direction == Vector2.Zero);
+            SpriteTexture = GameHost.Textures[asteroidTextures[GameHelper.RandomNext(0, 2)]];
+        }
+
+        public Asteroid(GameHost game, Vector2 position)
+            : this(game)
+        {
+            Position = position;
+        }
+
+        private Vector2 direction_ = new Vector2();
+
+        public Vector2 Direction
+        {
+            get { return direction_; }
+            set { direction_ = value; direction_.Normalize(); }
+        }
+
+        public float Speed
+        { get; set; }
+
+        public int Life
+        { get; set; }
+
+        public float RotateSpeed
+        { get; set; }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            Position += Direction * Speed;
+            Angle += MathHelper.ToRadians(RotateSpeed);
+
+            if (PositionX < OriginX)
+            {
+                PositionX = OriginX;
+                Direction *= new Vector2(-1f, 1f);         
+            }
+            else if (PositionX > GameHost.GraphicsDevice.Viewport.Bounds.Width - OriginX)
+            {
+                PositionX = GameHost.GraphicsDevice.Viewport.Bounds.Width - OriginX;
+                Direction *= new Vector2(-1f, 1f);         
+            }
+            else if (PositionY < OriginY)
+            {
+                PositionY = OriginY;
+                Direction *= new Vector2(1f, -1f);         
+            }
+            else if (PositionY > GameHost.GraphicsDevice.Viewport.Bounds.Height - OriginY)
+            {
+                PositionY = GameHost.GraphicsDevice.Viewport.Bounds.Height - OriginY;
+                Direction *= new Vector2(1f, -1f);         
+            }
+
+        }
+    }
+}
---
+++ SpaceShooter/SpaceShooter/Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoFramework;
+
+namespace SpaceShooter
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : MonoFramework.GameHost
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Textures.Add("Rock1", Content.Load<Texture2D>("Rock1"));
+            Textures.Add("Rock2", Content.Load<Texture2D>("Rock2"));
+            Textures.Add("Rock3", Content.Load<Texture2D>("Rock3"));
+
+
+            // initialize ateroids
+
+            for (int i = 0; i < 10; ++i)
+            { 
+                GameObjects.Add(new Asteroid(this, new Vector2(
+                    GameHelper.RandomNext(0, GraphicsDevice.Viewport.Bounds.Right),
+                    GameHelper.RandomNext(0, GraphicsDevice.Viewport.Bounds.Bottom)))
+                );
+            }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            UpdateAll(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            DrawSprites(gameTime, _spriteBatch);
+            DrawText(gameTime, _spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
